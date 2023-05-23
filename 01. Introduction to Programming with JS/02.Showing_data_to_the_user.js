c3f0e4c7011669a274fe025318599c6434ff0cda
swayamterode <swayam.programmer@gmail.com>
2023-05-23 20:55:16 +0530

    Showing data to the user

---
+++ 01. Introduction to Programming with JS/02.Showing_data_to_the_user.js
@@ -0,0 +1,8 @@
+console.log("Passing values directly into the `console.log()`");
+console.log(10);
+
+console.log("Passing values with variable declared into the `console.log()`");
+var name = "Swayam Terode";
+console.log(name);
+
+console.log(12,name,"Pune");
