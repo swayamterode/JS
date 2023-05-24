97d99c46a2eb50eec8ee2d7e9e92e549926073ba
swayamterode <swayam.programmer@gmail.com>
2023-05-25 04:06:29 +0530

    Code: Types of Operators

---
+++ 01. Introduction to Programming with JS/05.Operators.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 20;
+
+// Arithmetic Operations
+console.log("Addition: ", x + y);
+console.log("Subtraction: ", x - y);
+console.log("Division: ", x / y);
+console.log("Multiplication: ", x * y);
+console.log("Remainder: ", x % y);
+console.log("Exponets/Power: ", x ** y);
+
+// Assignment Operators
+let num = 100,
+  a = 2;
+console.log(num);
+console.log((num += 2));
+num = 100; // resigned to 100 👍
+console.log((num -= 2));
