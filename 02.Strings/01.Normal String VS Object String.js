58fb5bf4094d675cd6841ca5769eff38ab554930
swayamterode <swayam.programmer@gmail.com>
2023-06-14 02:51:44 +0530

    Code: String Vs String Constructor

---
+++ 02.Strings/01.Normal String VS Object String.js
@@ -0,0 +1,7 @@
+// declare two variables, firstName and lastName
+let firstName = "John";
+let lastName = new String("Wick"); // string constructor
+
+// print the value of lastName and its type
+console.log("\nString is", lastName, "----->", typeof lastName);
+console.log("\nString is", firstName, "----->", typeof firstName);
