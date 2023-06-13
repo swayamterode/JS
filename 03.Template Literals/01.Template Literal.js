58233e709712ca4c2145304b90062f99b826925c
swayamterode <swayam.programmer@gmail.com>
2023-06-14 02:59:42 +0530

    Code: Template Literal

---
+++ 03.Template Literals/01.Template Literal.js
@@ -0,0 +1,12 @@
+// A template literal in JavaScript is a string literal that allows you to
+// embed expressions and variables in a string.
+// They are enclosed in backticks (`) instead of double or single quotes.
+
+const message = `Dear Mam,
+
+My name is Swayam I am Final Year Student I want 1 of leave for JS preaparation
+
+Reagards
+Swayam Terode`;
+
+console.log(message);
